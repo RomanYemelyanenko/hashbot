72c72c548d532187324e993228daa02de99ff24c
Roman Yemelyanenko <roman.yemelyanenko@touchin.ru>
2013-06-28 11:36:39 +0400

    Рефакторинг MainScreanTabController.cs

--- IOS/IPhone/Screens/MainScreanTabController.cs
+++ IOS/IPhone/Screens/MainScreanTabController.cs
@@ -5,20 +5,14 @@ namespace HashBot
 {
 	public class MainScreanTabController : UITabBarController
 	{
-		private UIViewController twitterTab, dribbbleTab, appleTab, gihubTab;
-
 		public MainScreanTabController () 
 		{
-			twitterTab = CreateController ("#Twitter","Images/TabBar/icon_twitter.png");
-			dribbbleTab = CreateController ("#Dribbble","Images/TabBar/icon_dribbble.png");
-			appleTab = CreateController ("#Apple","Images/TabBar/icon_apple.png");
-			gihubTab = CreateController ("#GitHub","Images/TabBar/icon_github.png");
-
-			var tabs = new UIViewController[] {
-				twitterTab, dribbbleTab, appleTab, gihubTab
+			ViewControllers = new UIViewController[] {
+				CreateController ("#Twitter","Images/TabBar/icon_twitter.png"),
+				CreateController ("#Dribbble","Images/TabBar/icon_dribbble.png"),
+				CreateController ("#Apple","Images/TabBar/icon_apple.png"),
+				CreateController ("#GitHub","Images/TabBar/icon_github.png")
 			};
-
-			ViewControllers = tabs;
 		}
 
 
